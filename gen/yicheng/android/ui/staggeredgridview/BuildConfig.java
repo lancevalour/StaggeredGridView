12d003cfad65e6c0b7c7a4d72e24be60206e3802
Yicheng Zhang <zhangyicheng1234@gmail.com>
2015-05-29 13:40:22 -0400

    Initial commit

---
+++ gen/yicheng/android/ui/staggeredgridview/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package yicheng.android.ui.staggeredgridview;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
